fa36d5f8149e54b0f507c0506eb3e8721ac79ff6
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-11-02 11:35:53 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -12,45 +12,50 @@ resource "aws_vpc" "awsec2demo" {
 
 ## AWS VPC Subnet
 resource "aws_subnet" "awsec2demo" {
-    vpc_id = aws_vpc.awsec2demo.id
-    cidr_block = "172.16.10.0/24"
+  vpc_id = aws_vpc.awsec2demo.id
+  cidr_block = "172.16.10.0/24"
 
-    tags = {
-      Name = "subnet-quickcloudpocs"
-    }
+  tags = {
+    Name = "subnet-quickcloudpocs"
+  }
 }
 
 ## AWS Network Interface
 resource "aws_network_interface" "awsec2demo" {
-    subnet_id = aws_subnet.awsec2demo.id
-    private_ips = ["172.16.10.100"]
+  subnet_id    = aws_subnet.awsec2demo.id
+  private_ips = ["172.16.10.100"]
 
-tags = {
+  tags = {
     Name = "NI-quickcloudpocs"
-}
+  }
 }
 
 ## AWS Security Group
 resource "aws_security_group" "awsec2demo" {
-  name = "${random_pet.sg.id}-sg"
+  name  = "${random_pet.sg.id}-sg"
   vpc_id = aws_vpc.awsec2demo.id
+
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 ## AWS EC2
 resource "aws_instance" "awsec2demo" {
-    ami = "ami-0dbc3d7bc646e8516" # us-east-1
-    instance_type = "t2.micro"
- tags = {
-    Name = "MyEC2Instance"  # Specify the desired name for your EC2 instance
+  ami           = "ami-067c21fb1979f0b27" # ap-south-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance" # Specify the desired name for your EC2 instance
   }
-    network_interface {
-      network_interface_id = aws_network_interface.awsec2demo.id
-      device_index = 0
-    } 
+
+  network_interface {
+    network_interface_id = aws_network_interface.awsec2demo.id
+    device_index        = 0
+  }
+
+  associate_public_ip_address = true
 }
