5d1c7ca6c7f613cc77d0edae8f5322f215c4a4cc
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-11-02 11:45:03 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "awsec2demo" {
-  ami           = "ami-05c0f5389589545b7" # ap-south-1
+  ami           = "ami-01bc990364452ab3e" # us-east-1
   instance_type = "t2.micro"
 
   tags = {
